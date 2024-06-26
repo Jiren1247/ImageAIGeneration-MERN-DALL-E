02dd492cdb5cc08b3082509fb26e9b8a18dce317
Jiren1247 <jiren@vt.edu>
2024-06-26 17:45:12 -0400

    fix: upload the null build folder

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://imageaigeneration-mern-dall-e.onrender.com/api/v1/post', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json'
